5edd1c3e44559c5b8d2b8536af1534be727ccd54
B-8128 <brijeshvadaliya1@gmail.com>
2022-11-04 17:59:01 +0530

    Error Solved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom";
-import App from "./app.js";
+import App from "./App.js";
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('brijesh'));
